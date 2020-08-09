5ed89fccb051fee77c98b6699911c186ce118883
Coco <coco.li7777@gmail.com>
2020-08-09 15:08:48 +1000

    build left nav list and canva pro area

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
 const createBtn = document.getElementById('createBtn');
+const crownBtn = document.getElementById('crownBtn');
+const proBtn = document.getElementById('proBtn');
+const coverLayer = document.getElementById('cover');
 
 const toggleDropdown = () => {
   const dropdown = document.getElementById('designArea');
   dropdown.classList.toggle('show');
 };
 
+const addProPage = () => {
+  const proPage = document.getElementById('canvaPro');
+  proPage.classList.add('pro-show');
+  coverLayer.style.display = 'block';
+};
+const closePropage = () => {
+  const proPage = document.getElementById('canvaPro');
+  proPage.classList.remove('pro-show');
+  coverLayer.style.display = 'none';
+};
+
 createBtn.addEventListener('click', toggleDropdown);
+proBtn.addEventListener('click', closePropage);
+crownBtn.addEventListener('click', addProPage);
