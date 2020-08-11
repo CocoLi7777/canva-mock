ead8657148c4890fa587f0c62edf9d4dc4727cb7
Coco <coco.li7777@gmail.com>
2020-08-11 19:32:46 +1000

    make navbar responsively and add animation to left window

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const createBtn = document.getElementById('createBtn');
 const crownBtn = document.getElementById('crownBtn');
 const proBtn = document.getElementById('proBtn');
 const coverLayer = document.getElementById('cover');
+const leftBtn = document.getElementById('leftBtn');
 
 const toggleDropdown = () => {
   const dropdown = document.getElementById('designArea');
@@ -19,6 +20,28 @@ const closePropage = () => {
   coverLayer.style.display = 'none';
 };
 
+const closeCoverLayer = () => {
+  const leftShow = document.getElementById('leftShow');
+
+  leftShow.style.animationName = 'leftShowOut';
+  leftShow.style.animationDuration = '1s';
+  setTimeout(() => {
+    leftShow.style.display = 'none';
+  }, 1000);
+
+  coverLayer.style.display = 'none';
+};
+
+const openLeftShow = () => {
+  const leftShow = document.getElementById('leftShow');
+  leftShow.style.display = 'block';
+  leftShow.style.animationName = 'leftShowIn';
+  leftShow.style.animationDuration = '1s';
+  coverLayer.style.display = 'block';
+};
+
 createBtn.addEventListener('click', toggleDropdown);
 proBtn.addEventListener('click', closePropage);
 crownBtn.addEventListener('click', addProPage);
+leftBtn.addEventListener('click', openLeftShow);
+coverLayer.addEventListener('click', closeCoverLayer);
